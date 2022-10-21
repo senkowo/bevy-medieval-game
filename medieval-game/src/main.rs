977b51a515b13e04758d63e9c3482839aab99a05
senkowo <arntzkai@gmail.com>
2022-10-20 20:23:39 -0400

    test code with sprite

--- medieval-game/src/main.rs
+++ medieval-game/src/main.rs
@@ -1,3 +1,18 @@
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+// This code creates a blank window with a horse sprite in it.
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("horse1.png"),
+        ..default()
+    });
 }
