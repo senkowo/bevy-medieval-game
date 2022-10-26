b744c4620e67b633aaeb8c76164a7adb4c3e48dc
senkowo <arntzkai@gmail.com>
2022-10-26 19:21:59 -0400

    updated some code, still need to copy more over from the tutorial video

--- medieval-game/src/main.rs
+++ medieval-game/src/main.rs
@@ -1,18 +1,63 @@
+#![allow(unused)] // silence warnings given by the compiler saying that a variable is not in use. Delete this line later.
 
 use bevy::prelude::*;
 
+// region:    --- Asset Constants
+
+const PLAYER_SPRITE: &str = "horse1.png";
+const PLAYER_SIZE: (f32, f32) = (144., 75.);
+
+
+// endregion: --- Asset Constants
+
 fn main() {
     App::new()
+        // Default background color
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        // Customizing window.
+        .insert_resource(WindowDescriptor {
+            title: "Rust Invaders!".to_string(),
+            width: 600.0,
+            height: 600.0,
+            ..Default::default()
+        })
+        // Add default plugins to make the program run correctly.
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
+        // Add Startup systems below
+        .add_startup_system(setup_system)
+        // Run the program
         .run();
 }
 
-// This code creates a blank window with a horse sprite in it.
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+// This is the system (or function) that sets up the game.
+// The "commands" variable of type 'Commands' (from bevy::prelude::Commands) allows you
+//   to add or remove things from the game scene.
+// The "asset_server" of type 'Res<AssetServer>' is a resource (Res<_>) that loads resources
+//   from the filesystem (e.g. images for sprites) and injects them as an argument as
+//   appropriate (it's a Generic variable, so it'll adapt to load any file).
+// The "windows" variable of type 'ResMut<Windows>' is a resource (ResMut<_>) that will
+//   allow you to access and alter the window's components (properties).
+fn setup_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut windows: ResMut<Windows>,
+) {
+    // spawn camera entity
     commands.spawn_bundle(Camera2dBundle::default());
+    //commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // --- Capture window size
+    // Get window properties from the "windows" resource of type 'ResMut<Windows>'.
+    let window = windows.get_primary_mut().unwrap();
+    let (win_w, win_h) = (window.width(), window.height());
+
+    // position window (for tutorial)
+    window.set_position(IVec2::new(2780, 4900));
+
+    // Spawn Main sprite entity -- using SpriteBundle (sprite builder)
     commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("horse1.png"),
-        ..default()
+        // uses asset_server to load image and returns a handle (pointer?)
+        texture: asset_server.oad(PLAYER_SPRITE),
+        ..Default::default() // add default properties for the rest
     });
 }
